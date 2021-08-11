98aaf1946c30da7fc1a74b6ae8763b54fcda3a51
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-11 12:53:51 -0300

    criando componente cards

--- Pokemon/src/app/app.module.ts
+++ Pokemon/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PokemonListaComponent } from './pokemon-lista/pokemon-lista.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PokemonListaComponent
+    PokemonListaComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
