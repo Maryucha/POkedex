1c2a7a1707d1079b05aabde50436c3f7477a62e3
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-11 12:50:18 -0300

    criando o serviço

--- Pokemon/src/app/pokemon-lista/pokemon-lista.component.ts
+++ Pokemon/src/app/pokemon-lista/pokemon-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon-lista',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonListaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: PokemonService) { }
 
   ngOnInit(): void {
   }
